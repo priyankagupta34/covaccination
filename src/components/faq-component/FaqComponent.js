c74d288e9666292c7fc7d379bcb9c8392f7d8191
Gupta Priyanka (RBEI/ERD1) <Priyanka.Gupta@in.bosch.com>
2021-07-11 18:37:33 +0530

    first add

---
+++ src/components/faq-component/FaqComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class FaqComponent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
