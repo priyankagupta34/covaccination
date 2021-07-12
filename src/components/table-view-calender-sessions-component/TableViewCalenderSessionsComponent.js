51ca71d418de2b021f5958771f46d8dae421fa53
Gupta Priyanka (RBEI/ERD1) <Priyanka.Gupta@in.bosch.com>
2021-07-12 19:01:26 +0530

    basic outline

--- src/components/table-view-calender-sessions-component/TableViewCalenderSessionsComponent.js
+++ src/components/table-view-calender-sessions-component/TableViewCalenderSessionsComponent.js
@@ -21,35 +21,37 @@ export default class TableViewCalenderSessionsComponent extends Component {
                 </div>
 
                 <div className="tabs">
-                    <div className="hjab">
-                        <div className="grfsd">
-                            <div className="uih">Name</div>
-                            {/* <div className="uih">Address</div> */}
-                            <div className="uih">Fee Type </div>
-                            <div className="uih">Min Age</div>
-                            <div className="uih">All Slots</div>
-                            <div className="uih">Dose 1</div>
-                            <div className="uih">Dose 2</div>
+                    <div className="viab">
+                        <div className="hjab">
+                            <div className="grfsd">
+                                <div className="uih tys">Name</div>
+                                {/* <div className="uih">Address</div> */}
+                                <div className="uih">Fee Type </div>
+                                <div className="uih">Min Age</div>
+                                <div className="uih">All Slots</div>
+                                <div className="uih">Dose 1</div>
+                                <div className="uih">Dose 2</div>
+                            </div>
                         </div>
+                        {service.map((item, index) => (
+                            <div className="grfsd">
+
+                                {/* <div className="ui">{item.address}</div> */}
+
+                                {item.sessions.map((item2, index2) =>
+                                    <>
+                                        <div className="ui tys">{item.name}</div>
+                                        <div className="ui">{item.fee_type}</div>
+                                        <div className="ui">{item2.min_age_limit}</div>
+                                        <div className="ui">{item2.available_capacity}</div>
+                                        <div className="ui">{item2.available_capacity_dose1}</div>
+                                        <div className="ui">{item2.available_capacity_dose2}</div>
+                                    </>
+                                )}
+
+                            </div>
+                        ))}
                     </div>
-                    {service.map((item, index) => (
-                        <div className="grfsd">
-
-                            {/* <div className="ui">{item.address}</div> */}
-
-                            {item.sessions.map((item2, index2) =>
-                                <>
-                                    <div className="ui">{item.name}</div>
-                                    <div className="ui">{item.fee_type}</div>
-                                    <div className="ui">{item2.min_age_limit}</div>
-                                    <div className="ui">{item2.available_capacity}</div>
-                                    <div className="ui">{item2.available_capacity_dose1}</div>
-                                    <div className="ui">{item2.available_capacity_dose2}</div>
-                                </>
-                            )}
-
-                        </div>
-                    ))}
                 </div>
             </div>
         )
