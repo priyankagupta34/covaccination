c54305610e1d753f1b0da9229373780feaa4d72a
Priyanka Gupta <34priyankagupta@gmail.com>
2021-07-17 23:50:55 +0530

    adding proper table

--- src/components/display-slot-and-book-component/DisplaySlotAndBookComponent.js
+++ src/components/display-slot-and-book-component/DisplaySlotAndBookComponent.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
 import './DisplaySlotAndBookComponent.css'
-import { beneficiaries } from '../../services/test'
+// import { beneficiaries } from '../../services/test'
 
 export default class DisplaySlotAndBookComponent extends Component {
     render() {
         const { beneficiaries} = this.props;
         const { selectedCenter, selectedSession } = this.props;
         console.log('this.props', this.props);
-        console.log(beneficiaries);
+        console.log(beneficiaries, selectedSession);
         return (
             <div>
-                
+                <div>
+                    <div>Name</div>
+                    <div>{selectedCenter.name}</div>
+                </div>
             </div>
         )
     }
