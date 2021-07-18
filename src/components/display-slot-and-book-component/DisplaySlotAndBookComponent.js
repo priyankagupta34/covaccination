5e79fe972ac8d4df86de7086e953b0a551ab21b5
Priyanka Gupta <34priyankagupta@gmail.com>
2021-07-18 22:16:14 +0530

    adding proper table

--- src/components/display-slot-and-book-component/DisplaySlotAndBookComponent.js
+++ src/components/display-slot-and-book-component/DisplaySlotAndBookComponent.js
@@ -37,89 +37,94 @@ export default class DisplaySlotAndBookComponent extends Component {
                     <div className="paidr terts">INR {selectedCenter.fee_type === 'Paid' ? selectedCenter.vaccine_fees ? (selectedCenter.vaccine_fees.filter(a => a.vaccine === selectedSession.vaccine).length !== 0) ? selectedCenter.vaccine_fees.filter(a => a.vaccine === selectedSession.vaccine)[0]['fee'] : 'N/A' : 'N/A' : 0}</div>
                 </div>
 
-                <div>
-                    <div className="selectBene">Please select one beneficiary from below to proceed for vaccination slot booking</div>
-                    <div className="detailbenef">
-                        <div className="beneflist">
-                            {beneficiaries && beneficiaries.map((benef, index) => (
-                                <div className={`${index === selectedBeneficiary && 'selectedbenefnam'} benefnam`} onClick={this.selectdBenefHandler.bind(this, index)}>
-                                    <span className="checkits">{index === selectedBeneficiary ? <>&#9989;</> : <></>}</span>
-                                    {benef.name}
-                                </div>
+                {!beneficiaries.length ?
+                    <div>
+                        <div className="selectBene">Please select one beneficiary from below to proceed for vaccination slot booking</div>
+                        <div className="detailbenef">
+                            <div className="beneflist">
+                                {beneficiaries && beneficiaries.map((benef, index) => (
+                                    <div className={`${index === selectedBeneficiary && 'selectedbenefnam'} benefnam`} onClick={this.selectdBenefHandler.bind(this, index)}>
+                                        <span className="checkits">{index === selectedBeneficiary ? <>&#9989;</> : <></>}</span>
+                                        {benef.name}
+                                    </div>
 
-                            ))}
-                        </div>
-                        {beneficiaries &&
-                            <div className="benefdetails">
-                                {beneficiaries[selectedBeneficiary].vaccination_status === 'Partially Vaccinated' ?
-                                    <>
-                                        {CoServices.checkDateDifference(new Date(), CoServices.getTodaysDate(CoServices.getRightDateFromCowinFormat(beneficiaries[selectedBeneficiary]['dose1_date']), 84)) ?
-                                            <>
-
-                                                {selectedSession.available_capacity_dose2 > 0 ?
-                                                    <>
-                                                        <div className="koli">Elible Candidate. Please proceed by clicking on button below</div>
-                                                        <div className="proceedDiv"><button className="proceed">Proceed Booking...</button></div>
-                                                    </>
-                                                    :
-                                                    <>
-                                                        <div className="koli">Elible Candidate. But unfortunately slots are not available. Try later.</div>
-                                                        <div className="proceedDiv"><button className="proceed" disabled={true}>Booking not Possible</button></div>
-                                                    </>
-                                                }
-                                            </> :
-                                            <>
-                                                <div className="koli">Not Eligible Yet!! You still have {CoServices.checkNumberOfDaysLeftforDose2(beneficiaries[selectedBeneficiary]['dose1_date'])} Left for second dose</div>
-                                                <div className="proceedDiv"><button className="proceed" disabled={true}>Booking not Possible</button></div>
-                                            </>
-                                        }
-                                    </>
-                                    :
-                                    <>
-                                        {beneficiaries[selectedBeneficiary].vaccination_status === 'Not Vaccinated' ?
-
-                                            <>
-                                                {CoServices.checkIfAgeIsElibleAsperSlot(selectedSession['allow_all_age'], selectedSession['min_age_limit'], selectedSession['max_age_limit'], beneficiaries[selectedBeneficiary]['birth_year']) ?
-                                                    <>
-                                                        {selectedSession.available_capacity_dose1 > 0 ?
-                                                            <>
-                                                                <div className="koli">Elible Candidate. Please proceed by clicking on button below</div>
-                                                                <div className="proceedDiv"><button className="proceed">Proceed Booking...</button></div>
-                                                            </>
-                                                            :
-                                                            <>
-                                                                <div className="koli">Elible Candidate. But unfortunately slots are not available. Try later.</div>
-                                                                <div className="proceedDiv"><button className="proceed" disabled={true}>Booking not Possible</button></div>
-                                                            </>}
-                                                    </>
-                                                    :
-                                                    <>
-                                                        <div className="koli">Candidate's age is not eligible for this slot</div>
-                                                        <div className="proceedDiv"><button className="proceed" disabled={true}>Booking not Possible.</button></div>
-
-                                                    </>
-                                                }
-                                            </>
-                                            :
-                                            <>
-                                                {beneficiaries[selectedBeneficiary].vaccination_status === 'Vaccinated' ?
-
-                                                    <>
-                                                        <div className="koli">Glad to know you are vaccinated citizen. :) </div>
-                                                        <div className="proceedDiv"><button className="proceed" disabled={true}>Booking not Possible</button></div>
-                                                    </>
-                                                    :
-                                                    <></>
-                                                }
-                                            </>
-                                        }
-                                    </>
-                                }
+                                ))}
                             </div>
-                        }
+                            {beneficiaries &&
+                                <div className="benefdetails">
+                                    {beneficiaries[selectedBeneficiary].vaccination_status === 'Partially Vaccinated' ?
+                                        <>
+                                            {CoServices.checkDateDifference(new Date(), CoServices.getTodaysDate(CoServices.getRightDateFromCowinFormat(beneficiaries[selectedBeneficiary]['dose1_date']), 84)) ?
+                                                <>
+
+                                                    {selectedSession.available_capacity_dose2 > 0 ?
+                                                        <>
+                                                            <div className="koli">Elible Candidate. Please proceed by clicking on button below</div>
+                                                            <div className="proceedDiv"><button className="proceed">Proceed Booking...</button></div>
+                                                        </>
+                                                        :
+                                                        <>
+                                                            <div className="koli">Elible Candidate. But unfortunately slots are not available. Try later.</div>
+                                                            <div className="proceedDiv"><button className="proceed" disabled={true}>Booking not Possible</button></div>
+                                                        </>
+                                                    }
+                                                </> :
+                                                <>
+                                                    <div className="koli">Not Eligible Yet!! You still have {CoServices.checkNumberOfDaysLeftforDose2(beneficiaries[selectedBeneficiary]['dose1_date'])} Left for second dose</div>
+                                                    <div className="proceedDiv"><button className="proceed" disabled={true}>Booking not Possible</button></div>
+                                                </>
+                                            }
+                                        </>
+                                        :
+                                        <>
+                                            {beneficiaries[selectedBeneficiary].vaccination_status === 'Not Vaccinated' ?
+
+                                                <>
+                                                    {CoServices.checkIfAgeIsElibleAsperSlot(selectedSession['allow_all_age'], selectedSession['min_age_limit'], selectedSession['max_age_limit'], beneficiaries[selectedBeneficiary]['birth_year']) ?
+                                                        <>
+                                                            {selectedSession.available_capacity_dose1 > 0 ?
+                                                                <>
+                                                                    <div className="koli">Elible Candidate. Please proceed by clicking on button below</div>
+                                                                    <div className="proceedDiv"><button className="proceed">Proceed Booking...</button></div>
+                                                                </>
+                                                                :
+                                                                <>
+                                                                    <div className="koli">Elible Candidate. But unfortunately slots are not available. Try later.</div>
+                                                                    <div className="proceedDiv"><button className="proceed" disabled={true}>Booking not Possible</button></div>
+                                                                </>}
+                                                        </>
+                                                        :
+                                                        <>
+                                                            <div className="koli">Candidate's age is not eligible for this slot</div>
+                                                            <div className="proceedDiv"><button className="proceed" disabled={true}>Booking not Possible.</button></div>
+
+                                                        </>
+                                                    }
+                                                </>
+                                                :
+                                                <>
+                                                    {beneficiaries[selectedBeneficiary].vaccination_status === 'Vaccinated' ?
+
+                                                        <>
+                                                            <div className="koli">Glad to know you are vaccinated citizen. :) </div>
+                                                            <div className="proceedDiv"><button className="proceed" disabled={true}>Booking not Possible</button></div>
+                                                        </>
+                                                        :
+                                                        <></>
+                                                    }
+                                                </>
+                                            }
+                                        </>
+                                    }
+                                </div>
+                            }
+                        </div>
+
                     </div>
 
-                </div>
+                    :
+                    <></>
+                }
 
                 <div className="selectBene1">See complete details on beneficiaries below</div>
 
