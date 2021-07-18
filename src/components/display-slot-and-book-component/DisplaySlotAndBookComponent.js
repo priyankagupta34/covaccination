614801f413cf9180dd5cc5d140a2c720b5b71a91
Priyanka Gupta <34priyankagupta@gmail.com>
2021-07-18 14:18:39 +0530

    adding proper table

--- src/components/display-slot-and-book-component/DisplaySlotAndBookComponent.js
+++ src/components/display-slot-and-book-component/DisplaySlotAndBookComponent.js
@@ -4,17 +4,46 @@ import './DisplaySlotAndBookComponent.css'
 
 export default class DisplaySlotAndBookComponent extends Component {
     render() {
-        const { beneficiaries} = this.props;
+        const { beneficiaries } = this.props;
         const { selectedCenter, selectedSession } = this.props;
         console.log('this.props', this.props);
         console.log(beneficiaries, selectedSession);
         return (
-            <div>
-                <div>
-                    <div>Name</div>
-                    <div>{selectedCenter.name}</div>
+            <div className="slotdisp">
+                <div className="addrese">
+                    <div className="addname">{selectedCenter.name}</div>
+                    <div className="blockname">{selectedCenter.address}</div>
+                    <div className="blockname">{selectedCenter.block_name}, {selectedCenter.pincode}</div>
                 </div>
-            </div>
+
+                <div className="feeknow">
+                    <div className="ferts">Fee Type</div>
+                    <div className="terts">{selectedCenter.fee_type} </div>
+                    <div className="paidr terts">INR {selectedCenter.fee_type === 'Paid' ? selectedCenter.vaccine_fees ? (selectedCenter.vaccine_fees.filter(a => a.vaccine === selectedSession.vaccine).length !== 0) ? selectedCenter.vaccine_fees.filter(a => a.vaccine === selectedSession.vaccine)[0]['fee'] : 'N/A' : 'N/A' : 0}</div>
+                </div>
+
+
+
+
+                {/* {selectedCenter.fee_type === 'PaidHide' ? < div className="vaccinfee">
+                    <div className="vaccH">Vaccine</div>
+                    <div className="vaccH">Fee</div>
+                    {selectedCenter.vaccine_fees.map((fee) => (
+                        <>
+                            <div className="vacc vaccH">
+                                {fee.vaccine}
+                            </div>
+                            <div className="vacc">
+                                {fee.fee}
+                            </div>
+                        </>
+
+                    ))}
+                </div> : <></>}
+            
+             */}
+
+            </div >
         )
     }
 }
