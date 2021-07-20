478bb6fcce8750b0283c0271116c13162a184677
Priyanka Gupta <34priyankagupta@gmail.com>
2021-07-20 18:14:56 +0530

    adding proper table

--- src/components/display-slots-component/DisplaySlotsComponent.js
+++ src/components/display-slots-component/DisplaySlotsComponent.js
@@ -8,8 +8,10 @@ export default class DisplaySlotsComponent extends Component {
             <div className="slotlst">
                 {slotList.map((item, index) => (
                     <div key={index} onClick={() => selectSlotHandler(index)} className={selectedSlot === index ? 'selectedSlot' : 'slot'}>
+                        <span style={{marginRight: 3}}>{selectedSlot === index ? <>&#9989;</> : <>&#11036;</>}</span>
                         {item}
-                        {selectedSlot === index ?<div className="slotick">&#9989;</div>:<div className="slotick">&#11036;</div>}
+
+                        {/* {selectedSlot === index ?<div className="slotick">&#9989;</div>:<div className="slotick">&#11036;</div>} */}
                     </div>
                 ))}
             </div>
